815c7f1ed894eb3fa4dcafc43c5e2f418f510d11
S Sanjith <83284162+S-Sanjith@users.noreply.github.com>
2023-04-05 02:34:06 -0700

    Update find-s.py

--- find-s/find-s.py
+++ find-s/find-s.py
@@ -1,32 +1,3 @@
-# import csv
-
-# def updateHypothesis(x, h):
-#     if h == []:
-#         return x
-#     for i in range(0, len(h)):
-#         if x[i].upper() != h[i].upper():
-#             h[i] = '?'
-#     return h
-
-# if __name__ == '__main__':
-#     data = []
-#     h = []
-
-#     #read csv file
-#     with open('enjoysport.csv', 'r') as file:
-#         reader = csv.reader(file)
-#         print('Data:')
-#         for row in reader:
-#             data.append(row)
-#             print(row)
-
-#     if data:
-#         for x in data:
-#             if x[-1].upper() == 'YES':x.pop()
-#             h = updateHypothesis(x, h)
-
-#     print('\nHypothesis: ', h)
-
 import csv
 a = []
 with open('enjoysport.csv', 'r') as csvfile:
